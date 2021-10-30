12b7d3f4b02556d217ab5a8f4d0401c6ff83376f
R-Song <ryansong02@gmail.com>
2021-10-30 15:50:14 -0400

    Finished setup for testing scheduling code

--- examples/nack_scheduling.c
+++ examples/nack_scheduling.c
@@ -11,19 +11,22 @@ int test(cmph_uint32* items_to_hash, cmph_uint32 items_len, CMPH_ALGO alg_n)
     cmph_t *hash;
     cmph_config_t *config;
     cmph_io_adapter_t *source;
-    cmph_uint32 i;
     char filename[256];
     FILE* mphf_fd = NULL;
 
-    printf("%s (%u)\n", cmph_names[alg_n], alg_n);
+    source = cmph_io_struct_vector_adapter(
+        items_to_hash,
+        (cmph_uint32)sizeof(cmph_uint32),
+        0,
+        (cmph_uint32)sizeof(cmph_uint32),
+        items_len
+    );
 
-    source = cmph_io_struct_vector_adapter(items_to_hash,
-                                           (cmph_uint32)sizeof(cmph_uint32),
-                                           0,
-                                           (cmph_uint32)sizeof(cmph_uint32),
-                                           items_len);
     config = cmph_config_new(source);
     cmph_config_set_algo(config, alg_n);
+    cmph_config_set_graphsize(config, 5.00);
+    cmph_config_set_verbosity(config, 1);
+
     if (alg_n == CMPH_BRZ) {
         sprintf(filename, "%s_%u.mph", cmph_names[alg_n], items_len);
         mphf_fd = fopen(filename, "w");
@@ -32,22 +35,7 @@ int test(cmph_uint32* items_to_hash, cmph_uint32 items_len, CMPH_ALGO alg_n)
     hash = cmph_new(config);
     cmph_config_destroy(config);
 
-    if (alg_n == CMPH_BRZ) {
-        cmph_dump(hash, mphf_fd);
-        cmph_destroy(hash);
-        fclose(mphf_fd);
-        mphf_fd = fopen(filename, "r");
-        hash = cmph_load(mphf_fd);
-    }
-    printf("packed_size %u\n",cmph_packed_size(hash));
-
-    for (i=0; i<items_len; ++i)
-        printf("%d -> %u\n",
-               items_to_hash[i],
-               cmph_search(hash,
-                           (char*)(items_to_hash+i), 
-                           (cmph_uint32)sizeof(cmph_uint32)));
-    printf("\n");
+    printf("Packed Size: %u bytes for %u keys\n",cmph_packed_size(hash), items_len);
 
     cmph_io_vector_adapter_destroy(source);   
     cmph_destroy(hash);
@@ -68,11 +56,24 @@ int main (void)
 
     // Generate random vector of active users
     cmph_uint32 i;
+    cmph_uint32 T[total_users];
     for(i = 0; i < active_len; i++) {
         active[i] = rand() % total_users;
+        while(T[active[i]] == 1) {
+            active[i] = rand() % total_users;
+        }
+        T[active[i]] = 1;
+    }
+    for(i = 0; i < nack_len; i++) {
+        nack[i] = rand() % total_users;
+        while(T[nack[i]] == 1) {
+            nack[i] = rand() % total_users;
+        }
+        T[nack[i]] = 1;
     }
 
-    printf("TESTING: %u ACTIVE, %u NACKS, %u TOTAL\n", active_len, nack_len, total_users);
+    printf("CHDN: %u ACTIVE, %u NACKS, %u TOTAL\n", active_len, nack_len, total_users);
+    // TODO: Actually implement that algorithm...
     test(active, active_len, CMPH_CHD);
 
     return 0;
